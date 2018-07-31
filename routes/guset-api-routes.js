6897ed08aefa779d8ff5d96fdb5a12ce083ecd71
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-07-31 19:54:26 -0400

    update guests route

---
+++ routes/guset-api-routes.js
@@ -0,0 +1,56 @@
+var db = require("../models");
+module.exports = function(app) {
+  // start get
+  app.get("/api/guests/", function(req, res) {
+    if (req.params.id) {
+      db.guests.findById(req.params.id).then(function(guest) {
+        res.json(guest);
+      });
+    } else {
+      db.guests.findAll({}).then(function(guest) {
+        res.json(guest);
+      });
+    }
+  });
+  // end get
+
+  // start post
+  app.post("/api/guests", function(req, res) {
+    db.guests.create(req.body).then(function(guest) {
+      res.json(guest);
+    });
+  });
+  // end post
+
+  // start put
+  app.put("/api/guests", function(req, res) {
+    db.guests.update({
+        text: req.body.text,
+          complete: req.body.complete
+        },
+        {
+          where: {
+            id: req.body.id
+          }
+        }
+      )
+      .then(function(guest) {
+        res.json(guest);
+      });
+  });
+  // end put
+
+  // start delete
+  app.delete("/api/guests/:id", function(req, res) {
+    db.guests
+      .destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(guest) {
+        res.json(guest);
+      });
+  });
+  // end delete
+};

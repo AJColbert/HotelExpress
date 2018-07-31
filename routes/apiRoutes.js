a92b975102192d1acc62c9ac3bcde81ebc3e6828
hamad alsulaiman <Hamad@hamadsMacBook-2.local>
2018-07-31 17:42:42 -0400

    update guests api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,55 @@
 var db = require("../models");
-
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+
+// start get
+  app.get("/api/guests", function (req, res) {
+    if (req.params.id) {
+      db.guests.findById(req.params.id).then(function (guest) {
+        res.json(guest);
+      });
+    } else {
+      db.guests.findAll({}).then(function (guest) {
+        res.json(guest);
+      });
+    }
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // end get
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+// start post
+  app.post("/api/guests", function (req, res) {
+    db.guests.create(req.body).then(function (guest) {
+      res.json(guest);
     });
   });
+  // end post
+
+// start put
+  app.put("/api/guests", function (req, res) {
+    // Use the sequelize update method to update a todo to be equal to the value of req.body
+    // req.body will contain the id of the todo we need to update
+    db.guests.update({
+      text: req.body.text,
+      complete: req.body.complete
+    }, {
+        where: {
+          id: req.body.id
+        }
+      }).then(function (guest) {
+        res.json(guest);
+      });
+  });
+// end put
+
+// start delete
+  app.delete("/api/guests/:id", function(req, res) {
+    db.guests.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(guest) {
+      res.json(guest);
+    });
+  });
+  // end delete
 };

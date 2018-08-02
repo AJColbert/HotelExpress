ef805cde4df89f264d6c7d62b2cb19579bf76eb2
Aaron Colbert <aaron-colbert@hotmail.com>
2018-08-02 19:01:07 -0400

    clean up

--- routes/booking-api-routes.js
+++ routes/booking-api-routes.js
@@ -35,10 +35,12 @@ module.exports = function (app)
     });
 
     // booking route for saving a new booking 
-    //TODO: Write BOOKING POST SEQUELIZE
     app.post("/api/bookings", function (req, res)
     {
         console.log(req.body);
+
+
+        
         db.booking.create(req.body)
             .then(function (dbBooking)
             {
